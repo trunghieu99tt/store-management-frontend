35f98a6e282b39f2962be1096cb716fe759c29ec
Nguyen Trung Hieu <trunghieu99tt@gmail.com>
2021-05-24 12:38:56 +0700

    fix fetching data

--- src/components/FinancialForecasting/FinancialForecasting.tsx
+++ src/components/FinancialForecasting/FinancialForecasting.tsx
@@ -16,145 +16,157 @@ import Loading from "../Loading";
 import defaultClasses from "./financialForecasting.module.css";
 
 interface Props {
-  classes?: object;
+    classes?: object;
 }
 
 const columns = [
-  {
-    title: "Tháng",
-    dataIndex: "month",
-    key: "month",
-  },
-  {
-    title: "Lợi nhuận",
-    dataIndex: "profit",
-    key: "profit",
-    render: (value: number) => {
-      return <strong>{formatNumber(value)}</strong>;
+    {
+        title: "Tháng",
+        dataIndex: "month",
+        key: "month",
     },
-  },
+    {
+        title: "Lợi nhuận",
+        dataIndex: "profit",
+        key: "profit",
+        render: (value: number) => {
+            return <strong>{formatNumber(value)}</strong>;
+        },
+    },
 ];
 
 const FinancialForecasting = ({ classes: propsClasses }: Props) => {
-  const classes = mergeClasses(defaultClasses, propsClasses);
-  const [revenues, setRevenues] = useState<iRevenue[] | any>(null);
-  const [expenses, setExpenses] = useState<iExpense[] | any>(null);
+    const classes = mergeClasses(defaultClasses, propsClasses);
+    const [revenues, setRevenues] = useState<iRevenue[] | any>(null);
+    const [expenses, setExpenses] = useState<iExpense[] | any>(null);
+    const [data, setData] = useState<any>(null);
+    const [forecast, setForecast] = useState<any>(null);
 
-  const { fetchAllRevenues, fetchRevenuesInRange } = useRevenue();
-  const { fetchExpenseInRange } = useExpense({ type: "BASE" });
-  const { fetchExpenses } = useExpense({ type: "BASE" });
+    const { fetchAllRevenues, fetchRevenuesInRange } = useRevenue();
+    const { fetchExpenseInRange } = useExpense({ type: "BASE" });
+    const { fetchExpenses } = useExpense({ type: "BASE" });
 
-  const handleFetchAllExpenses = async () => {
-    const data = await fetchExpenses();
-    setExpenses(data.data);
-  };
+    const handleFetchAllExpenses = async () => {
+        const data = await fetchExpenses();
+        setExpenses(data.data);
+    };
 
-  const handleFetchAllRevenues = async () => {
-    const data = await fetchAllRevenues();
-    setRevenues(data);
-  };
+    const handleFetchAllRevenues = async () => {
+        const data = await fetchAllRevenues();
+        setRevenues(data);
+    };
 
-  useEffect(() => {
-    handleFetchAllRevenues();
-    handleFetchAllExpenses();
-  }, []);
+    useEffect(() => {
+        handleFetchAllRevenues();
+        handleFetchAllExpenses();
+        handleFetchData();
+    }, []);
 
-  // if(!revenues || !expenses){
-  //     return <Loading />
-  // }
+    const handleFetchData = async () => {
+        const { data, forecast } = await getFinancialData(
+            fetchRevenuesInRange,
+            fetchExpenseInRange
+        );
+        setData(data);
+        setForecast(forecast);
+    };
 
-  const revenueCount = revenues?.reduce(
-    (prev: any, curr: any) => prev + curr.total,
-    0
-  );
-  const expenseCount = expenses?.reduce(
-    (prev: any, curr: any) => prev + curr.total,
-    0
-  );
-  const profit = revenueCount - expenseCount;
+    // if(!revenues || !expenses){
+    //     return <Loading />
+    // }
 
-  const { data, forecast } = getFinancialData(fetchRevenuesInRange, fetchExpenseInRange);
+    const revenueCount = revenues?.reduce(
+        (prev: any, curr: any) => prev + curr.total,
+        0
+    );
+    const expenseCount = expenses?.reduce(
+        (prev: any, curr: any) => prev + curr.total,
+        0
+    );
+    const profit = revenueCount - expenseCount;
 
-  return (
-    <section className={classes.root}>
-      <OverviewCard name="Doanh thu" number={revenueCount || 0} />
-      <br />
-      <OverviewCard name="Lợi nhuận" number={profit || 0} />
-      <br />
-      <Table
-        columns={columns}
-        dataSource={data}
-        pagination={{
-          pageSize: 12,
-          total: 12,
-        }}
-        scroll={{ x: "500px" }}
-        loading={!revenues || !expenses}
-      />
-      <p>
-        Dự báo năm {new Date().getFullYear() + 1}: Tăng trưởng {forecast} %
-      </p>
-    </section>
-  );
+    return (
+        <section className={classes.root}>
+            <OverviewCard name="Doanh thu" number={revenueCount || 0} />
+            <br />
+            <OverviewCard name="Lợi nhuận" number={profit || 0} />
+            <br />
+            <Table
+                columns={columns}
+                dataSource={data}
+                pagination={{
+                    pageSize: 12,
+                    total: 12,
+                }}
+                scroll={{ x: "500px" }}
+                loading={!revenues || !expenses || !data}
+            />
+            <p>
+                Dự báo năm {new Date().getFullYear() + 1}: Tăng trưởng{" "}
+                {forecast} %
+            </p>
+        </section>
+    );
 };
 
 const getTotalRevenues = (monthlyRevenues: any) => {
     return monthlyRevenues?.reduce(
         (prev: any, curr: any) => prev + curr.total,
         0
-      );
-}
+    );
+};
 
 const getTotalExpenses = (monthlyExpenses: any) => {
     return monthlyExpenses?.reduce(
         (prev: any, curr: any) => prev + curr.total,
         0
-      );
-}
+    );
+};
 
-const getFinancialData = (fetchRevenuesInRange: any, fetchExpenseInRange: any) => {
-    
-  const currentYear = new Date().getFullYear();
-  const getRevenueByMonth = async (monthNumber: number) => {
-    let month = monthNumber.toString();
-    if (monthNumber < 10) {
-      month = "0" + monthNumber;
-    }
-    const dateFrom = moment(`${month}${currentYear}`, "MMYYYY")
-      .toDate()
-      .toLocaleDateString();
-    const dateTo = moment(dateFrom)
-      .endOf("month")
-      .toDate()
-      .toLocaleDateString();
+const getFinancialData = async (
+    fetchRevenuesInRange: any,
+    fetchExpenseInRange: any
+) => {
+    const currentYear = new Date().getFullYear();
+    const getRevenueByMonth = async (monthNumber: number) => {
+        let month = monthNumber.toString();
+        if (monthNumber < 10) {
+            month = "0" + monthNumber;
+        }
+        const dateFrom = moment(`${month}${currentYear}`, "MMYYYY")
+            .toDate()
+            .toLocaleDateString();
+        const dateTo = moment(dateFrom)
+            .endOf("month")
+            .toDate()
+            .toLocaleDateString();
 
-    // console.log("dateFrom", dateFrom);
-    // console.log("dateTo", dateTo);
+        // console.log("dateFrom", dateFrom);
+        // console.log("dateTo", dateTo);
 
-    const revenues = await fetchRevenuesInRange(dateFrom, dateTo);
-    const expenses = await fetchExpenseInRange(dateFrom, dateTo);
+        const revenues = await fetchRevenuesInRange(dateFrom, dateTo);
+        const expenses = await fetchExpenseInRange(dateFrom, dateTo);
 
-    const profit = getTotalRevenues(revenues.data) - getTotalExpenses(expenses.data)
-    return profit;
-    // console.log('data', data)
-  };
-
-  let data: any = [];
-  let forecast = 0;
-  for (let i = 1; i < 12; i++) {
-    let value = 0;
-    getRevenueByMonth(i).then((r) => {
-        value = r;
-        console.log('value', value)
-    });
-    const item = {
-      month: `Tháng ${i} -> Tháng ${i + 1}`,
-      profit: value,
+        const profit =
+            getTotalRevenues(revenues.data) - getTotalExpenses(expenses.data);
+        return profit;
+        // console.log('data', data)
     };
-    console.log('value', value)
-    data.push(item);
-  }
-  return { data, forecast };
+
+    let data: any = [];
+    let forecast = 0;
+    for (let i = 1; i < 12; i++) {
+        let value = 0;
+        await getRevenueByMonth(i).then((r) => {
+            value = r;
+        });
+        const item = {
+            month: `Tháng ${i} -> Tháng ${i + 1}`,
+            profit: value,
+        };
+        data.push(item);
+    }
+    return { data, forecast };
 };
 
 export default FinancialForecasting;
